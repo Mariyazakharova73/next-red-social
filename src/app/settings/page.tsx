5f17ad022eaad8c7f3dfe43d3e7a66ef280e525c
Mariya Zakharova <zakharovamaria73@yandex.ru>
2024-01-09 10:59:29 +0400

    chat

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,13 +1,22 @@
+import Button from '@/components/ui/button/Button';
+import { RoutePath } from '@/utils/config';
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
 	title: 'Settings',
 	description: '',
 };
 
-
 const SettingsPage = () => {
-	return <div>SettingsPage</div>;
+	return (
+		<div className='p-layout flex flex-col gap-2'>
+			<h1>Страница в разработке</h1>
+			<Link href={RoutePath.chats}>
+				<Button>На главную</Button>
+			</Link>
+		</div>
+	);
 };
 
 export default SettingsPage;
